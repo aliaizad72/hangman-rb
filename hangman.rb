1e73891e32524a35ab200ec97df54323cf78f11e
Ali Aizad <aliaizad72@gmail.com>
2024-02-18 21:16:26 +0800

    Add player_guess attribute to store player's current guess

--- hangman.rb
+++ hangman.rb
@@ -2,12 +2,14 @@
 
 # serves as the class that holds the game states
 class Game
+  attr_accessor :player_guess
+
+  attr_reader :secret_word
+
   def initialize
     @secret_word = Wordlist.random_word './vocab.txt'
+    @player_guess = '_' * secret_word.length # Initial player guess is none correct
   end
-end
-
-class Player
 end
 
 # serves as a class to provide the words for the game
@@ -16,5 +18,3 @@ class Wordlist
     File.open(path).readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }.sample
   end
 end
-
-puts Wordlist.random_word './vocab.txt'
