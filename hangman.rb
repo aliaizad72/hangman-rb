8df86b3c560e278019fa5babd6bc7df47f7f5a59
Ali Aizad <aliaizad72@gmail.com>
2024-02-18 21:36:25 +0800

    Add a few lines of string for intro to game

--- hangman.rb
+++ hangman.rb
@@ -10,6 +10,19 @@ class Game
     @secret_word = Wordlist.random_word './vocab.txt'
     @player_guess = '_' * secret_word.length # Initial player guess is none correct
   end
+
+  def play
+    intro
+  end
+
+  def intro
+    puts 'We have sentence you to death...but if you are able to guess the mystery word,'
+    puts "you will escape unscathed.\n\n"
+    puts 'You will have eight rounds.'
+    puts 'You can either guess a letter in the word, or the whole word itself.'
+    puts 'If your letter guess is right, we will reveal to you where the letters reside in the word.'
+    puts 'If you choose to guess a word and you are able to guess correctly, you are free immediately.'
+  end
 end
 
 # serves as a class to provide the words for the game
@@ -18,3 +31,5 @@ class Wordlist
     File.open(path).readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }.sample
   end
 end
+
+Game.new.play
