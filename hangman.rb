b74927076caf850d6b3aa5f429bf405996b733da
Ali Aizad <aliaizad72@gmail.com>
2024-02-18 17:54:55 +0800

    Changed the random word to a class method

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Game
+  def initialize
+    @secret_word = Dictionary.random_word
+  end
 end
 
 class Player
@@ -8,15 +11,9 @@ end
 
 # serves as a class to provide the words for the game
 class Dictionary
-  attr_reader :wordlist
-
-  def initialize(path)
-    @wordlist = File.open(path).readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }
-  end
-
-  def random_word
-    @wordlist.sample
+  def self.random_word
+    File.open('./vocab.txt').readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }.sample
   end
 end
 
-puts Dictionary.new('./vocab.txt').random_word
+puts Dictionary.random_word
