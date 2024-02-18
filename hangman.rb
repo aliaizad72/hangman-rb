7935ecdcf78c220ca58c0b9f8d75e084d84acf03
Ali Aizad <aliaizad72@gmail.com>
2024-02-18 17:06:38 +0800

    Add class Dictionary for word list creation and word selection

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
-list_file = File.open('./vocab.txt')
+class Game
+end
 
-word_list = list_file.readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }
+class Player
+end
+
+# serves as a class to provide the words for the game
+class Dictionary
+  attr_reader :wordlist
+
+  def initialize(path)
+    @wordlist = File.open(path).readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }
+  end
+
+  def random_word
+    @wordlist.sample
+  end
+end
+
+puts Dictionary.new('./vocab.txt').random_word
