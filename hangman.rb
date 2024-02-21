b8b7caa342c121010c850024cb660bc2106b3cb2
Ali Aizad <aliaizad72@gmail.com>
2024-02-21 22:13:54 +0800

    Allow players to guess words, and announce winners/losers after game ends

--- hangman.rb
+++ hangman.rb
@@ -7,28 +7,42 @@ class Game
   attr_reader :secret_word, :player
 
   def initialize
-    @secret_word = Wordlist.random_word './vocab.txt'
+    @secret_word = Wordlist.random_word './words.txt'
     @player = Player.new(secret_word)
     intro
     @round = 1
   end
 
   def play
-    until round == 9 || player.guess == secret_word
+    until round == 11 || player.guess == secret_word
       puts "Round #{round}:"
       puts "Mystery word: #{player.guess_display}\n\n"
       player.update_guess
       @round += 1
     end
+
+    if player.guess == secret_word
+      announce_winner
+    else
+      announce_death
+    end
   end
 
   def intro
-    puts 'We have sentence you to death...but if you are able to guess the mystery word,'
+    puts "#{player.name}, we have sentence you to death...but if you are able to guess the mystery word,"
     puts "you will escape unscathed.\n\n"
-    puts 'You will have eight rounds.'
+    puts 'You will have ten rounds.'
     puts 'You can either guess a letter in the word, or the whole word itself.'
     puts 'If your letter guess is right, we will reveal to you where the letters reside in the word.'
     puts "If you choose to guess a word and you are able to guess correctly, you are free immediately.\n\n"
+  end
+
+  def announce_winner
+    puts 'Congratulations, the gods have given you another chance at life.'
+  end
+
+  def announce_death
+    puts "The word that killed you was '#{secret_word}'. Unfortunate, but you had your chance."
   end
 end
 
@@ -45,7 +59,7 @@ class Player
   end
 
   def ask_name
-    print 'Prisoner, enter your name: '
+    print 'State your name, prisoner: '
     gets.chomp
   end
 
@@ -55,8 +69,10 @@ class Player
 
   def update_guess
     print "#{name}, enter your guess: "
-    input = gets.chomp
+    input = gets.chomp.downcase
+    puts
     replace_chars(input) if word.count_chars.keys.include?(input)
+    @guess = input if @word == input
   end
 
   def replace_chars(char)
